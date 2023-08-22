6611fc2bb0d9eae6d8a610ce8a05e998d7cb321b
Vanessa Ponte <vanessaponte@gmail.com>
2023-08-22 13:56:30 -0300

    feat: single page application

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+function getContent(fragmentId) {
+  const pages = {
+    home: "This is the Home page. Welcome to my site.",
+    about: "This page will describe what my site is about",
+    contact: "Contact me on this page if you have any questions",
+  };
+  return pages[fragmentId];
+}
+
+function loadContent() {
+  const contentDiv = document.getElementById("app");
+  const fragmentId = location.hash.substring(1);
+  contentDiv.innerHTML = getContent(fragmentId);
+}
+
+if (!location.hash) {
+  location.hash = "#home";
+}
+
+loadContent();
+
+window.addEventListener("hashchange", loadContent);
