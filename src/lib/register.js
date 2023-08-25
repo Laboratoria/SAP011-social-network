0733e4cea36f5f5ccb857592445e1f4f5b3bb76b
Vanessa Ponte <vanessaponte@gmail.com>
2023-08-25 20:18:42 -0300

    feat testes de SPA para register

--- src/lib/register.js
+++ src/lib/register.js
@@ -1,6 +1,38 @@
-const register = registerSreen () => {
-    
-        `
-
-        `
-}
+export default () => {
+  const content = `
+                <h1>Crie sua conta</h1>
+                <p><a href="">Já é registrado? Faça Login.</a></p>
+        
+                <div id="form-container">
+                <img src="./img/photo-profile.png" alt="foto de perfil ilustrativa" />
+                <form action="">
+                <label for="name">Nome</label>
+                <input class="input" type="text" id="nomeId" required name="name" />
+        
+                <label for="email">E-mail</label>
+                <input class="input"
+                type="text"
+                id="emailId"
+                placeholder="seunome@email.com"
+                required
+                name="email"
+                />
+        
+                <label for="password">Senha</label>
+                <input class="input" type="password" id="passwordId" required name="password" />
+        
+                <label for="date">Data de nascimento</label>
+                <input class="input" type="date" id="birthId" />
+        
+                <input id="submit" type="submit" class="send" value="Enviar" />
+                </form>
+        
+                </div>
+        
+                <div class="decoration" id="green-ball"></div>
+                <div class="decoration" id="purple-ball">
+                <img src="./img/purple-ball.png" alt="" />
+                </div>
+                <p>by Julia Ribeiro e Vanessa Ponte</p>
+        `;
+};
--- src/main.js
+++ src/main.js
@@ -6,25 +6,32 @@
 
 import { register } from "./lib/register.js";
 
-function getContent(fragmentId) {
-  const pages = {
-    home: register,
-    about: "This page will describe what my site is about",
-    contact: "Contact me on this page if you have any questions",
-  };
-  return pages[fragmentId];
+function fillMain() {
+  const mainContent = document.getElementById("app");
+  mainContent.innerHTML = register;
 }
 
-function loadContent() {
-  const contentDiv = document.getElementById("app");
-  const fragmentId = location.hash.substring(1);
-  contentDiv.innerHTML = getContent(fragmentId);
-}
+fillMain();
 
-if (!location.hash) {
-  location.hash = "#home";
-}
+// function getContent(fragmentId) {
+//   const pages = {
+//     home: register,
+//     about: "This page will describe what my site is about",
+//     contact: "Contact me on this page if you have any questions",
+//   };
+//   return pages[fragmentId];
+// }
 
-loadContent();
+// function loadContent() {
+//   const contentDiv = document.getElementById("app");
+//   const fragmentId = location.hash.substring(1);
+//   contentDiv.innerHTML = getContent(fragmentId);
+// }
 
-window.addEventListener("hashchange", loadContent);
+// if (!location.hash) {
+//   location.hash = "#home";
+// }
+
+// loadContent();
+
+// window.addEventListener("hashchange", loadContent);
