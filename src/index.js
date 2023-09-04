968272c078062d5c9edbbd5eb35e21121a47e878
Vanessa Ponte <vanessaponte@gmail.com>
2023-09-04 10:21:39 -0300

    build: firebase auth
    Co-authored-by: Vanessa <vanessaponte@gmail.com>
    Co-authored-by: Julia <juliamvribeiro@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+
+const firebaseApp = {
+  apiKey: "AIzaSyA41Dab9eY4PHp_wsvUVa9e8nYCsfLSUl8",
+  authDomain: "mypet-network.firebaseapp.com",
+  projectId: "mypet-network",
+  storageBucket: "mypet-network.appspot.com",
+  messagingSenderId: "15817347931",
+  appId: "1:15817347931:web:647f11598bc19b133a407e",
+  measurementId: "G-M7X1CYLD8K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseApp);
+const auth = getAuth(app);
