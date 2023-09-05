dd5512a58dcc1d81e4ea6822f80a4a8540941b47
ribeirober1208 <ribeirober1208@gmail.com>
2023-09-04 21:21:53 -0300

    add spa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
-// Este es el punto de entrada de tu aplicacion
+import home from "./pages/home/home.js";
+import login from "./pages/login/login.js";
+import criar from "./pages/criar/criar.js";
+import favorite from "./pages/favorite/favorite.js";
 
-// import { myFunction } from './lib/index.js';
+const main = document.querySelector("#root");
+const int = () => {
+    window.addEventListener("hashchange", () => { //mudança de rota hashchange
+        main.innerHTML = "";
+        switch (window.location.hash) {
+            case " ":
+                main.appendChild(home());
+                break;
+            case "#login":
+                main.appendChild(login());
+                break;
+            case "#criar":
+                main.appendChild(criar());
+                break;
+            case "#favorite":
+                main.appendChild(favorite());
+                break;
+            default:
+                main.appendChild(home());
+        }
 
-// myFunction();
+    })
+
+}
+
+window.addEventListener("load", () => {
+    main.appendChild(home());
+    int();
+})
