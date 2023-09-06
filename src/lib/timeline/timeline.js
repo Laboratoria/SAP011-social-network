780943225dbed5339f71f43f6bf993aeb97aa3f3
Vanessa Ponte <vanessaponte@gmail.com>
2023-09-05 22:56:05 -0300

    build: barra de ferramentas feed

--- src/lib/timeline/timeline.js
+++ src/lib/timeline/timeline.js
@@ -1,12 +1,20 @@
+const screen = `
+      <section id="main-timeline">
+        <div id="div-logo">
+            <img src="./img/logo-top-timeline.png" alt="MyPet logo">
+        </div>
+        <div id="feed">
+          <p>Aqui ficam as postagens</p>
+        </div>
+        <div id="functions">
+          <a href="#login"><img src="./img/feed-exit-icon.png" alt=""></a>
+          <a href=""><img src="./img/feed-search-icon.png" alt=""></a>
+          <a href=""><img src="./img/feed-post-icon.png" alt=""></a>
+          <a href=""><img src="./img/feed-profile-icon.png" alt=""></a>
+        </div>
+      </section>`;
+
 export default () => {
   const container = document.getElementById("app");
-  const screen = `
-    <section id="main-timeline">
-      <div id="div-logo">
-          <img src="./img/logo-top-timeline.png" alt="MyPet logo">
-      </div>
-      <p> Estamos na timeline </p>
-      <a href="#login">Voltar para login</a>
-    </section>`;
   container.innerHTML = screen;
 };
