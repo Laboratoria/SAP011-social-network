f542e9120e1f8235932888e5598821f75cce455a
Vanessa Ponte <vanessaponte@gmail.com>
2023-09-09 13:15:15 -0300

    feat: timeline: criação de post, formatação, scroll do feed

--- src/lib/timeline/timeline.js
+++ src/lib/timeline/timeline.js
@@ -6,18 +6,19 @@ const screen = `
             <button id="signout-button" type="button"><img class="logout" src="./img/exit-icon.png" alt=""></button>
             
         </div>
-        <div id="container">
 
           <div id="feed">
 
             <div id="feed-container">
-              <p>Ainda não temos postagens por aqui.</p>
-              <img src="./img/angry-cat.gif" alt="Cat Meme">
-              <p>Seja o primeiro!</p>
+              <div id="empty-feed">
+                <p>Ainda não temos postagens por aqui.</p>
+                <img src="./img/angry-cat.gif" alt="Cat Meme">
+              </div>
             </div>
 
             <div id="input-container">
               <input type="text" name="post" placeholder="Publique algo" id="postPlace"/>
+              <button id="publish-button" type="button"><img src="./img/send-button-white.png" alt="Send Button"></button>
             </div>
 
           </div>
@@ -28,11 +29,10 @@ const screen = `
 
             <button id="search-button" type="button"><img src="./img/feed-search-icon.png" alt="sair"></button>
 
-            <button id="post-button" type="button"><img src="./img/feed-post-icon.png" alt=""></button>
+            <button id="my-post-button" type="button"><img src="./img/feed-post-icon.png" alt="minhas postagens"></button>
 
-            <button id="profile-button" type="button"><img src="./img/feed-profile-icon.png" alt=""></button>
+            <button id="profile-button" type="button"><img src="./img/feed-profile-icon.png" alt="meu perfil"></button>
 
-          </div>
         </div>
       </section>`;
 
@@ -46,13 +46,45 @@ export default () => {
     feed.scrollTo(0, 0);
   });
 
-  const postButton = document.getElementById("post-button");
+  const postButton = document.getElementById("publish-button");
   const postPlace = document.getElementById("postPlace");
 
   function postIt() {
     console.log(postPlace.value);
-    if (postPlace.value !== "") {
-      feed.innerHTML = postPlace.value;
+    const erased = document.getElementById("empty-feed");
+
+    if (postPlace.value !== "" && erased) {
+      const post = document.createElement("div");
+      post.innerHTML = postPlace.value;
+      post.style.width = "90%";
+      post.style.height = "fit-content";
+      post.style.display = "flex";
+      post.style.flexWrap = "wrap";
+      post.style.backgroundColor = "#fff";
+      post.style.margin = "5px 0";
+      post.style.padding = "5px";
+      post.style.alignItems = "center";
+
+      document.getElementById("feed-container").appendChild(post);
+
+      erased.parentNode.removeChild(erased);
+
+      postPlace.value = "";
+    } else if (postPlace.value !== "" && !erased) {
+      const post = document.createElement("div");
+      post.innerHTML = postPlace.value;
+      post.style.width = "90%";
+      post.style.height = "fit-content";
+      post.style.display = "flex";
+      post.style.flexWrap = "wrap";
+      post.style.backgroundColor = "#fff";
+      post.style.margin = "5px 0";
+      post.style.padding = "5px";
+      post.style.alignItems = "center";
+
+      document.getElementById("feed-container").appendChild(post);
+
+      postPlace.value = "";
     }
   }
 
