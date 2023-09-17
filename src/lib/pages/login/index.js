a7f6b891da58c75c0d305af31621a921862e7425
emilainesantos <emilaine95@icloud.com>
2023-09-17 03:08:14 -0300

    css tela login

--- src/lib/pages/login/index.js
+++ src/lib/pages/login/index.js
@@ -3,12 +3,13 @@ export const login = () => {
   container.innerHTML = `
     <!--<form action="feed.html>" --!>
     <form class="login-container"> 
-      <h3 class="titulo">LOGIN<h3>
+      <h3 class="titulo">LOGIN</h3>
         <label>E-mail:</label>
           <input name="email" class="email" placeholder="seu@email.com">
           <div class="error">E-mail e obrigatorio</div>
           <div class="error">E-mail ivalido</div>
           <br>
+
         <label>Senha:</label>
           <input name="password" class="key" placeholder="digite sua senha.">
           <div class="error">Senha e obrigatoria</div>
@@ -16,6 +17,7 @@ export const login = () => {
           <button type="button" class="solid" disable="true"id="register" value="cadastrar">Cadastrar</button>
           <button type="button" class="clear" disable="true" id="recover">recuperar senha</button>
           <br>
+          
           <label>Preencher com o:</label>
           <button id="Gmail" >Gmail</button>
     </form>
