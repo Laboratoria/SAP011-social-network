0e7f44b3cd3fe422d597818831fdf1e8baa59ada
Leticia Bezerra <leticia21bezerradearaujo@gmail.com>
2023-09-28 20:41:12 -0300

    fix: page register (spa)

--- src/lib/pages/login/index.js
+++ src/lib/pages/login/index.js
@@ -6,13 +6,13 @@ export const login = () => {
       <h3 class="titulo">LOGIN</h3>
         <label class="config-titulo">E-mail:</label>
           <input name="email" class="email-input" placeholder="seu@email.com">
-          <div class="email error-none">E-mail e obrigatorio e invalido</div>
+          <div class = "error-none" id="email-error">E-mail e obrigatorio ou invalido</div>
     
           <br>
 
         <label class="config-titulo">Senha:</label>
           <input type="password" class="key" placeholder="Digite sua senha.">
-          <div class="password error-none">Senha esta invalida e é obrigatoria </div>
+          <div class="error-none" id="password-error">Senha esta invalida e é obrigatoria </div>
           
           <button type="button" class="email error" disabled id="enter">Entrar</button>
 
@@ -27,6 +27,8 @@ export const login = () => {
   // /*VALIDAÇÃO EM JAVA SCRIPT PARA PAG-LOGIN */
   const email = container.querySelector(".email-input");
   const password = container.querySelector(".key");
+  const emailMessage = container.querySelector("#email-error");
+  const passwordMessage = container.querySelector("#password-error");
 
   const validateEmail = container.querySelector(".email-input");
   validateEmail.addEventListener("input", function (e) {
@@ -43,56 +45,59 @@ export const login = () => {
     togglePasswordErrors(password);
   });
 
-
   const enterButton = container.querySelector("#enter");
   
-
   function toggleEmailErrors(email) {
     enterButton.addEventListener("click", function (e) {
       e.preventDefault();
-      if (email.value==="" || isValidEmail(email))  {
-        email.classList.remove("error-none");
-        email.classList.add("error-block");
+      if (email.value=== "" || isValidEmail(email))  {
+        emailMessage.classList.contains("error-none");
+        emailMessage.classList.remove("error-none");
+        emailMessage.classList.add("error-block");
       } else {
-        email.classList.add("error-block");
-        email.classList.remove("error-none");
+        emailMessage.classList.add("error-block");
+        emailMessage.classList.remove("error-none");
       }
+      console.log(emailMessage);
     });
   }
 
-  
   function togglePasswordErrors() {
     enterButton.addEventListener("click", function (e) {
       e.preventDefault();
       const password = container.querySelector(".key");
       if (password.value.length < 8) {
-        password.classList.remove("error-none");
-        password.classList.add("error-block");
+        passwordMessage.classList.remove("error-none");
+        passwordMessage.classList.add("error-block");
       } else {
-        password.classList.add("error-block");
-        password.classList.remove("error-none");
+        passwordMessage.classList.add("error-block");
+        passwordMessage.classList.remove("error-none");
       }
     });
   }
 
-
   function toggleButtonsDisable(email, password) {
-    const emailNotValid = isValidEmail(email);
-    const passwordNotValid = isValidPassword(password);
-    if (emailNotValid && passwordNotValid) {
-      enterButton.disabled = true;
+    const emailValid = isValidEmail(email.value);
+    const passwordValid = isValidPassword(password.value);
+    if (emailValid && passwordValid) {
+      enterButton.removeAttribute("disabled");
     } else {
-        enterButton.removeAttribute("disabled");
+      enterButton.disabled = true;
     }
   }
 
   function isValidEmail(email) {
    const parameter = /^\S+@\S+\.\S+$/;
-   return parameter.test(email); //retorna falso  
+   console.log(parameter.test(email));
+   return parameter.test(email); //retorna falso 
+   
   }
   
   function isValidPassword(password) {
+    console.log(password.length >= 8, password);
      return password.length >= 8; //false sempre
+     
   }
+
   return container;
 };
--- src/lib/pages/register/index.js
+++ src/lib/pages/register/index.js
@@ -1,76 +1,145 @@
 export const register = () => {
   const container = document.createElement("section");
-  container.innerHTML = `
-  <form class="registro-container"> 
-      <h3 class="titulo">BEM VINDO(A) A MEDGREEN<h3>
-      <label class="config-titulo">Digite seu nome completo:</label>
-        <input type="name" class="name" placeholder="Nome completo">
-        <br>
-      <label class="config-titulo">Nascimento:</label>
-          <input type="date" id="date" class="form">
-          <div class="date error-none">Data e obrigatorio</div>
-      <label class="config-titulo">E-mail:</label>
-        <br>
-          <input type="email" class="email-input" placeholder="seu@e-mail.com">
-          <div class="email error-none">E-mail e obrigatorio</div>
-      <label class="config-titulo">Senha:</label>
-          <input type="password" class="key" placeholder="Digite sua senha.">
-          <div class="password error-none">Senha e obrigatorio</div>
-          <button type="button" class="confirm" disabled id="enter" value="cadastrar">Cadastrar</button>
-      <label class="config-titulo">Faça login com:</label>
-          <button type="button" id="gmail">Google</button>
+    container.innerHTML = `
+    <form class="registro-container"> 
+        <h3 class="titulo">BEM VINDO(A) A MEDGREEN<h3>
+        <label class="config-titulo">Digite seu nome completo:</label>
+          <input type="name" class="name" placeholder="Nome completo">
+          <div class="error-none" id="name">Data e obrigatorio</div>
+          <br>
+        <label class="config-titulo">Nascimento:</label>
+            <input type="date" id="date" class="form">
+            <div id ="date" class="error-none">Data e obrigatorio</div>
+        <label class="config-titulo">E-mail:</label>
+          <br>
+            <input type="email" class="email-input" placeholder="seu@e-mail.com">
+            <div class="error-none" id ="email-error">E-mail e obrigatorio</div>
 
-          <a href="/#login">Pagina inicial</a>
-    </form>
-    `;
+        <label class="config-titulo">Senha:</label>
+            <input type="password" class="key" placeholder="Digite sua senha.">
+            <div class="error-none" id="password-error">Senha e obrigatorio</div>
 
-  const validateField = container.querySelector(".key");
-  validateField.addEventListener('input', function (e) {
-    e.preventDefault();
-    toggleButtonsDisable();
-    toggleFormsErrors();
-  }
-  );
+            <button type="button" class="confirm" disabled id="enter" value="cadastrar">Cadastrar</button>
 
-  const errorForm = container.querySelector('.key'); //validar campo santeriores para disparar 
-  const enterButton = container.querySelector('.confirm');
-  function toggleFormsErrors() { //validar campos antes de mandar para firebase
-    enterButton.addEventListener('click', function (e) {
+        <label class="config-titulo">Faça login com:</label>
+        
+            <button type="button" id="gmail">Google</button>
+
+            <a href="/#login">Pagina inicial</a>
+      </form>
+      `;
+
+    const email = container.querySelector(".email-input");
+    const password = container.querySelector(".key");
+    const date = container.querySelector("#date")
+    const emailMessage = container.querySelector("error-none");
+    const passwordMessage = container.querySelector("error-none");
+    const dateMessage = container.querySelector(".error-none")
+    const enterButton = container.querySelector("#enter");
+  
+    const validateEmail = container.querySelector(".email-input");
+    validateEmail.addEventListener("input", function (e) {
       e.preventDefault();
-      if (errorForm.classList.contains("error-none")) {
-        menu.classList.remove("error-none");
-        menu.classList.add("error-block");
-      } else {
-        menu.classList.remove("error-block");
-        menu.classList.add("error-none");
-      }
-    })
-  };
-
-  function toggleButtonsDisable() {
-    const passwordNotValid = isValidPassword();
-    if(passwordNotValid){
-      enterButton.disabled = true;
-    }else{
+      toggleButtonsDisable(email, password, date);
+      toggleEmailErrors(email);
   
-      enterButton.removeAttribute("disabled");
-    }
+    });
+  
+    const validatePassword = container.querySelector(".key");
+    validatePassword.addEventListener("input", function (e) {
+      e.preventDefault();
+      toggleButtonsDisable(email, password, date);
+      togglePasswordErrors(password);
+    });
+
+    const validateDate = container.querySelector(".form");
+    validateDate.addEventListener("input", function (e) {
+      e.preventDefault();
+      toggleButtonsDisable(email, password, date);
+      toggleDateErrors(date);
+  
+    });
     
-  };
-
-  function isValidPassword() {
-    const password = container.querySelector(".key").value;
-    if (password) {
-      return false;
-    } else {
-      return validatePassword(password);
-    }
-  };
-
-  function validatePassword(password) {
-    return /^\S{8}$/.test(password); //length
-  };
-
-  return container;
+    function toggleEmailErrors(email) {
+      enterButton.addEventListener("input", function (e) {
+        e.preventDefault();
+        if (email.value=== "" || isValidEmail(email))  {
+          emailMessage.classList.contains("error-none");
+          emailMessage.classList.remove("error-none");
+          emailMessage.classList.add("error-block");
+        } else {
+          emailMessage.classList.add("error-block");
+          emailMessage.classList.remove("error-none");
+        }
+        console.log(emailMessage);
+      });
+    };
   
+    
+    function togglePasswordErrors(password) {
+      enterButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        if (password.value.length < 8) {
+          passwordMessage.classList.remove("error-none");
+          passwordMessage.classList.add("error-block");
+        } else {
+          passwordMessage.classList.add("error-block");
+          passwordMessage.classList.remove("error-none");
+        }
+      });
+    };
+  
+    function toggleDateErrors(date){
+      enterButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        if (date.value.length < 8) {
+          dateMessage.classList.remove("error-none");
+          dateMessage.classList.add("error-block");
+        } else {
+          dateMessage.classList.add("error-block");
+          dateMessage.classList.remove("error-none");
+        }
+        console.log(date.value);
+      })
+    };
+
+    function toggleButtonsDisable(email, password) {
+      const emailValid = isValidEmail(email.value);
+      const passwordValid = isValidPassword(password.value);
+      const dateValid = isValidDate(date.value);
+      if (emailValid && passwordValid && dateValid) {
+        enterButton.removeAttribute("disabled");
+      } else {
+        enterButton.disabled = true;
+      }
+    }
+
+    function isValidEmail() {
+      const parameter = /^\S+@\S+\.\S+$/;
+      console.log(parameter.test(email));
+      return parameter.test(email); 
+    }
+     
+    function isValidPassword() {
+      console.log(password.length >= 8, password);
+      return password.length >= 8;  
+    }
+
+    function isValidDate() {
+      const ageMin = 18; 
+      const currentDate = new Date(); 
+      const currentYear = currentDate.getFullYear();
+      const age = currentYear - date.value;
+      console.log(birthYear)
+      if(age >= ageMin){
+        return true
+      }
+
+    }
+
+  return container
 };
+  
+   
+  
+
