7832ba31888aca751f240f118ab16913fabe50a4
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-09-28 09:12:57 -0300

    identação na main da branch dev

--- src/main.js
+++ src/main.js
@@ -4,24 +4,24 @@ import feed from "./pages/feed/feed.js";
 const main = document.querySelector("#root");
 
 const init = () => {
-    window.addEventListener("hashchange", () => {
-        main.innerHTML = "";
-    switch(window.location.hash){
-        case " ":
-            main.appendChild(home());
-            break;
-        case "#about":
-            main.appendChild(about());
-            break;
-        case "#feed":
-            main.appendChild(feed());
-            break;
-            default:
-                main.appendChild(home());
+  window.addEventListener("hashchange", () => {
+    main.innerHTML = "";
+    switch (window.location.hash) {
+      case " ":
+        main.appendChild(home());
+        break;
+      case "#about":
+        main.appendChild(about());
+        break;
+      case "#feed":
+        main.appendChild(feed());
+        break;
+      default:
+        main.appendChild(home());
     }
-})
-}
+  });
+};
 window.addEventListener("load", () => {
-    main.appendChild(home());
-    init();
+  main.appendChild(home());
+  init();
 });
