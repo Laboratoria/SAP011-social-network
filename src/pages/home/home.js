5b3a4d24348c32ccf201708e3760fdaf8179774b
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-09-29 11:00:23 -0300

    correção de identação, mudança das aspas duplas para simples nas strings

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,13 @@
 export default () => {
-    const container = document.createElement("div");
+  const container = document.createElement('div');
 
-    const template = `
+  const template = `
     <form>
     <input type= "email" placeholder="E-mail" name="email"/>
     <input type="password" placeholder="Senha" name="senha"/>
     </form>
       `;
 
-    container.innerHTML = template;
-    return container;
-    };
+  container.innerHTML = template;
+  return container;
+};
