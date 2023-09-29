8d03c880e3b16ac7afad98b9b8a991167cb5357b
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-09-28 21:28:08 -0300

    estudo sobre SPA, colocando a funcionalidade em prática nesta branch desativada

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,17 @@
 export default () => {
-    const container = document.createElement("div");
+  const container = document.createElement('div'); //a div adiciona todo o template
 
-    const template = `
+  const template = `
+      <body>
+        <header>
+          <img src="src/img/logotipo.png" alt="Logotipo">
+        </header>
+        
       <h2>titulo</h2>
       <p>paragrafo</p></>
+      </body>
       `;
 
-    container.innerHTML = template;
-    return container;
-    };
+  container.innerHTML = template;
+  return container;
+};
