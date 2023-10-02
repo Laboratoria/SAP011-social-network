8676b71d72888756b61476def7a71b807ad186e4
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-10-02 14:13:31 -0300

    alterações no about e criação da pasta register com register.js

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -17,11 +17,22 @@ export default () => {
       indique restaurantes, suas proprietárias e 
       histórias de luta e crescimento, 
       vamos incentivar e exaltar a potência de estabelecimentos locais.</p>
+
+      <p>Desenvolvido por
+
+      Aline Vieira
+      
+      Alycia Gonçalves
+      
+      Pamela Dándolo
+      
+      para Laboratória 2023
+      </p>
       </section>
       `;
 
-  // window.querySelector("#teste").addEventListener("click") () => {
-  //     window.location.hash = "#feed";
+  // window.querySelector("#about").addEventListener("click") () => {
+  //     window.location.hash = "#about";
   // };
   container.innerHTML = template;
   return container;
