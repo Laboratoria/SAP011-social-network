8166f0e1a1246bb3fd0e6c796ca1157e3e3ee5a6
Alycia <alyekaka@gmail.com>
2023-10-02 20:17:24 -0300

    pagina de registro

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -2,7 +2,24 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-    <h1> Teste caraio</h1>
+    <section>
+    <form id="cadastro-name">
+    <label>Nome:</label>
+    <input type="texto" placeholder="Digite seu nome..." name="nome"/>
+  </form>
+  <form id="cadastro-email"> 
+  <label>Email</label>
+  <input type= "email" placeholder="seu@email.com" name="email"/>
+</form>
+  <form id="cadastro-password">
+  <label>Senha:</label>
+  <input type="password" placeholder= "Crie sua senha..." name="senha"/>
+</form>
+<form id="confirmação-password">
+<label>Confirmação:</label>
+<input type="password" placeholder= "Confirme sua senha..." name="senha"/>
+</form>
+</section>
     `;
   container.innerHTML = template;
   return container;
