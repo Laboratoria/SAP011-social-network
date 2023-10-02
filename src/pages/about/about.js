077ea1701265f92859c25a546a4bd2b16bcc5ba2
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-10-02 13:49:51 -0300

    texto do about

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -2,10 +2,25 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-      <h2>about</h2>
-      <p>conteudo</p>
+      <section id="about">
+      <h2>Sobre nós</h2>
+
+      <p>Pessoas que buscam conhecer novos restaurantes e 
+      desfrutar da culinária brasileira e ao mesmo tempo desejam apoiar 
+      micro empreendedoras do ramo.
+
+      RestauranteDelas é uma rede social que faz a ponte entre você 
+      pessoa apreciadora da boa comida e mulheres que empreendem.
+      
+    
+      Compartilhe suas experiências através de postagens, 
+      indique restaurantes, suas proprietárias e 
+      histórias de luta e crescimento, 
+      vamos incentivar e exaltar a potência de estabelecimentos locais.</p>
+      </section>
       `;
-  // window.querySelector("#teste").addEventListener("click"); () => {
+
+  // window.querySelector("#teste").addEventListener("click") () => {
   //     window.location.hash = "#feed";
   // };
   container.innerHTML = template;
