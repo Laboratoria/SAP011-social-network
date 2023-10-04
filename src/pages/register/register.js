6fba98a00fd9b65c4637634362524fe471f5badb
Alycia <alyekaka@gmail.com>
2023-10-04 12:00:54 -0300

    merge da dev

---
+++ src/pages/register/register.js
@@ -0,0 +1,29 @@
+export default () => {
+  const container = document.createElement('div');
+
+  const template = `
+    <section>
+    <form id="cadastro-name">
+    <label>Nome:</label>
+    <input type="texto" placeholder="Digite seu nome..." name="nome"/>
+  </form>
+  <form id="cadastro-email"> 
+  <label>Email</label>
+  <input type= "email" placeholder="seu@email.com" name="email"/>
+</form>
+  <form id="cadastro-password">
+  <label>Senha:</label>
+  <input type="password" placeholder= "Crie sua senha..." name="senha"/>
+</form>
+<form id="confirmação-password">
+<label>Confirmação:</label>
+<input type="password" placeholder= "Confirme sua senha..." name="senha"/>
+</form>
+<form id ="google-button">
+<button> Botão do Google </button>
+</form>
+</section>
+    `;
+  container.innerHTML = template;
+  return container;
+};
