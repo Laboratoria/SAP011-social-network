b0923d6e9de2c173d26f4cd468f9f04cca82efa3
Aline Vieira <alinevieiraqq@gmail.com>
2023-10-04 12:36:59 -0300

    pagina do feed

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -2,8 +2,47 @@ export default () => {
   const container = document.createElement('div');
 
   const template = `
-      <h2>feed</h2>
-      <p>conteudo</p></>
+<header> 
+<p>
+<button type="submit">Sobre</button> <button type="submit">Sair</button> 
+</header>
+<body>
+<label>Nome</label>
+<p>
+<textarea placeholder="Digite seu post aqui..." minlength="20"
+maxlength="200" rows=5 cols=20>
+</textarea>
+<p>
+<button type="submit">Postar</button><img width="16" height="16" src="https://img.icons8.com/ios/50/hearts--v1.png" alt="hearts--v1"/>
+</form>
+<p>
+<label>Nome</label>
+<p>
+<textarea placeholder="Digite seu post aqui..." minlength="20"
+maxlength="200" rows=5 cols=20>
+</textarea>
+<p>
+<button type="submit">Postar</button><img width="16" height="16" src="https://img.icons8.com/ios/50/hearts--v1.png" alt="hearts--v1"/>
+</form>
+<p>
+<label>Nome</label>
+<p>
+<textarea placeholder="Digite seu post aqui..." minlength="20"
+maxlength="200" rows=5 cols=20>
+</textarea>
+<p>
+<button type="submit">Postar</button><img width="16" height="16" src="https://img.icons8.com/ios/50/hearts--v1.png" alt="hearts--v1"/>
+</form>
+<p>
+<label>Nome</label>
+<p>
+<textarea placeholder="Digite seu post aqui..." minlength="20"
+maxlength="200" rows=5 cols=20>
+</textarea>
+<p>
+<button type="submit">Postar</button><img width="16" height="16" src="https://img.icons8.com/ios/50/hearts--v1.png" alt="hearts--v1"/>
+</form>
+
       `;
 
   container.innerHTML = template;
