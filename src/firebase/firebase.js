5ad54673add0ad3277ffd69504ab6035b04acd41
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-10-09 17:19:24 -0300

    alreração de local de algumas funções de firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,23 @@
 // funções de autenticação e armazenamento (storage) do firebase
 import {
-    // entrar com google
-    getAuth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
+  // entrar com google
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
 
 } from 'firebase/auth';
 
 // função login
 
 export function login(email, password) {
-    const auth = getAuth();
-    return signInWithEmailAndPassword(auth, email, password);
+  const auth = getAuth();
+  return signInWithEmailAndPassword(auth, email, password);
 }
 
 // função para cadastrar usuário
 export function registerEmail(email, password) {
-    const auth = getAuth();
-    return createUserWithEmailAndPassword(auth, email, password);
+  const auth = getAuth();
+  return createUserWithEmailAndPassword(auth, email, password);
 }
 
 
