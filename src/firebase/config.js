60bcb88dfae5289ccc117d12777f21de9bbc8d31
Alycia <alyekaka@gmail.com>
2023-10-09 16:29:12 -0300

    alteraçoes da pagina register

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
   appId: '1:742669874515:web:2b867facd9a39a70805da6',
   measurementId: 'G-PN29GLXYNE',
 };
+firebase.initializeApp(firebaseConfig);
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
