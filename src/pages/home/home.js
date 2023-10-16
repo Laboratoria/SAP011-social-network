836308a45d5a6f899be219fc7378b1aff3c6fd91
PamelitaDandolo <pamela.senhoradosventos@gmail.com>
2023-10-16 10:40:43 -0300

    merge entre dev e SPA branches

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,6 @@
 // página com botões de email, senha, google e cadastrar
 import logotipo from '../../img/logo.png';
-import { login, registerEmail } from '../../firebase/firebase';
+import { login } from '../../firebase/firebase';
 
 // importar cada função de cada ação, login e senho, login pelo google
 export default () => {
