07461976bb4ff8bb997d3927587e9a35fd8a5422
Alycia <alyekaka@gmail.com>
2023-10-23 10:42:00 -0300

    merge apagar-post e editar

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,12 +1,3 @@
-import { 
-  collection, 
-  query, 
-  onSnapshot, 
-  orderBy,
-  doc,
-  updateDoc, 
-} from 'firebase/firestore'; 
-// collection são o conjunto de post lá em narnia
 import
 {
   collection,
@@ -19,33 +10,30 @@ import { db } from './config.js';
 // import { collection, addDoc } from 'firebase/firestore';
 // function sendPost
 
-export function lerPosts(exibirPosts) {
-console.log('lendo posts');
-  const q = query(collection(db, 'posts'));
+export function lerPosts(exibirPosts) {	
+  const q = query(collection(db, "posts"));	
 
-  // onSnapshot atualiza em tempo real
-  const posts = [];
-  const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    querySnapshot.forEach((doc) => { // pega cada um dos documentos que está em nárnia
-      posts.push(doc.data().text);
-    });
-    console.log('Posts', posts.join(''));
-    // console.log('Posts', posts);
-    // são os posts que vieram de narnia e foram jogados numa array que está dentro da const linha 9
-    // exibirPosts(posts);  nao é p firestore que deve exibir o post e sim o feed
-    // ao inves de console uma funçao que exibe os posts em tela
-  });
-  return posts; // exportar para o feed a função de ler posts
-}
 
-// editPost
-export const editPost = async (userId, newText, dataPost) => {
-  await updateDoc(doc(db, 'posts', userId), {
-    dataDoPost: dataPost,
-    textoDoPost: newText,
-    idUser: userId,
-  });
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {	
+    const posts = [];	
+    querySnapshot.forEach((doc) => {	
+      posts.push(doc.data().text);	
+    });	
+    console.log("Posts", posts.join(", "));	
+    exibirPosts(posts);	
+    // ao inves de console uma funçao que exibe os posts em tela	
+  })	
 };
 
-// likePost (Aline)
-// deletePost (Alycia)
+// createPost
+// export async function createPost() {
+//   const docRef = await addDoc(collection(db, 'posts'), {
+//     data: ' ',
+//     text: ' ',
+//   });
+//   console.log('Nova Postagem', docRef.id);
+// }
+
+// deletePost
+
+// Add a new document with a generated id.
--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -27,15 +27,16 @@ export default () => {
 maxlength="200" rows=5 cols=20>
 </textarea>
 <p>
-<button id="post-btn" type="submit">Postar</button><img width="16" height="16" src="https://img.icons8.com/ios/50/hearts--v1.png" alt="hearts--v1"/>
+<button id="post-btn" type="submit">Postar</button>/
+<p>
+<i class="material-icons custom-icon like" id="like">
+<a href="/#feed">favorite</a></i> <!-- Ícone de like do post -->
+<p>
 <section id ='posts'></section>
-
-
       `;
 
   container.innerHTML = template;
-  // escrever as postagens que o usuário escreve na tela
-  container.querySelector('#post-btn').addEventListener('lick', () => {
+  container.querySelector('#post-btn').addEventListener('click', () => {
     const newPost = container.querySelector('#new-post-txt').value;
     // createPost()
     console.log(newPost);
@@ -45,11 +46,13 @@ maxlength="200" rows=5 cols=20>
 
   function exibirPost(posts) {
     const postsExibir = container.querySelector('#posts');
-    const containerPosts = `
- <label for="containerPosts"></label>
- <textarea id="containerPosts" minlength="20" maxlength="200" rows=5 cols=20>${posts}</textarea>
- `;
-    postsExibir.innerHTML = containerPosts;
+    posts.forEach((post) => {
+      const containerPosts = `
+   <label for="containerPosts"></label>
+   <textarea id="containerPosts" minlength="20" maxlength="200" rows=5 cols=20>${post}</textarea>
+   `;
+      postsExibir.innerHTML += containerPosts;
+    });
   }
 
   // botão sair
