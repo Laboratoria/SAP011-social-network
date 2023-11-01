bfc262c1c987c18cc47baf65268be96a4e3de8a6
Alycia <alyekaka@gmail.com>
2023-11-01 00:31:41 -0300

    mexi na estilizaçao da  home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,25 +6,19 @@ import { login } from '../../firebase/firebase';
 export default () => {
   const container = document.createElement('div'); // adiciona o template
   const template = `
-  <header>
+<header>
   <img class="logo-header" id="logotipo" src=${logotipo} alt="Mulher segurando um prato com alimentos">
 </header>
 <br>
 <section class="center-content"> <!-- Usar a classe para centralizar o conteúdo -->
-  <form id="email"> 
-    <label>Email: </label>
+  <form id="email">
     <input type="email" id="email-login" placeholder=" seu@email.com" name="email"/>
   </form>
   <br>
   <form id="email-password">
-    <label>Senha: </label>
     <input type="password" id="password-login" placeholder=" digite a senha" name="senha"/>
   </form>
-  <br>
-  ou
-  <form>
-    <button id="google-button">Botão do Google</button>
-  </form>
+
   <br>
   <form id="register-login">
     <button id="register-btn">Cadastrar</button>
@@ -35,13 +29,7 @@ export default () => {
 
   container.innerHTML = template; // coloco o conteúdo do template dentro do innerhtml
 
-  // fazer um botão para logout dentro do inner container
 
-  // email e senha
-
-  // botão do google
-
-  // botão cadastrar
   const registerBtn = container.querySelector('#register-btn'); // variável que pega a ação do botão
   registerBtn.addEventListener('click', (event) => { // cadastrar e cria a página de cadastro
     event.preventDefault();
